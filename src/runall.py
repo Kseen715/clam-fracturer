5425d4de5de940fd1d1fccb4dc1f83968e1c9c97
kseen <korenevdn@gmail.com>
2024-08-06 21:04:32 +0300

    Refactor runall.py to include sorting of README.md

--- src/runall.py
+++ src/runall.py
@@ -1,9 +1,11 @@
 from sort_db import sort_db
+from sort_readme import sort_readme
 from maskt2ipv4 import maskt2ipv4
 from make_amnezia import make_amnezia
 
 
 if __name__ == '__main__':
     # maskt2ipv4()
+    sort_readme()
     sort_db()
     make_amnezia()
